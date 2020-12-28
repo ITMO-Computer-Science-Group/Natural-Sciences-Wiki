1d3c4d3cfea5f024d1944c5febe1432703fcab32
Ran-ying <62641267+Ran-ying@users.noreply.github.com>
2020-12-28 23:13:34 +0800

    update

--- washWord/washWord/Program.cs
+++ washWord/washWord/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Collections;
 
 namespace washWord
 {
@@ -6,7 +8,32 @@ namespace washWord
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            char[] upper = new char[] { };
+            char[] lower = new char[] { };
+            Console.WriteLine("Starting Washing Word");
+            ArrayList list = new ArrayList();
+            using (StreamReader sr = new StreamReader(@$"{Environment.CurrentDirectory}\\Законы сохранения в механике.md"))
+            {
+                string line = "";
+                int i = 0;
+                while ((line = sr.ReadLine())!=null)
+                {
+                    if (line.Length != 0 && line[0] == '|')
+                        if (!(line.Contains("русский") || line.Contains("-----")))
+                        {
+                            Console.WriteLine($"{i}\t{line.Split('|')[1].Trim()}\t{line.Split('|')[2].Trim()}");
+                            list.Add($"{i++}\t{line.Split('|')[1].Trim()}\t{line.Split('|')[2].Trim()}");
+                        }
+                }
+            }
+            using(StreamWriter sr = new StreamWriter($@"{Environment.CurrentDirectory}\\wordlist"))
+            {
+                foreach(string line in list)
+                {
+                    sr.WriteLine(line);
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
