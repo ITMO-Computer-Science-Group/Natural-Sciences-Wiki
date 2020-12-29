e5427ab893c538ef0c9b5c04dc510ebc63ef3119
Ran-ying <62641267+Ran-ying@users.noreply.github.com>
2020-12-29 15:36:16 +0800

    update

--- washWord/washWord/Program.cs
+++ washWord/washWord/Program.cs
@@ -8,31 +8,43 @@ namespace washWord
     {
         static void Main(string[] args)
         {
+            var files = Directory.GetFiles($"{Environment.CurrentDirectory}\\raw");
             char[] upper = new char[] { };
             char[] lower = new char[] { };
             Console.WriteLine("Starting Washing Word");
             ArrayList list = new ArrayList();
-            using (StreamReader sr = new StreamReader(@$"{Environment.CurrentDirectory}\\Законы сохранения в механике.md"))
+            string[] filename = new string[files.Length];
+
+            foreach (var file in files)
             {
-                string line = "";
-                int i = 0;
-                while ((line = sr.ReadLine())!=null)
+                int index = int.Parse(file.Split("\\")[file.Split("\\").Length-1].Split(' ')[0]);
+                filename[index] = file;
+            }
+            int i = 0;
+            foreach (var file in filename)
+            {
+                using (StreamReader sr = new StreamReader(file))
                 {
-                    if (line.Length != 0 && line[0] == '|')
-                        if (!(line.Contains("русский") || line.Contains("-----")))
-                        {
-                            Console.WriteLine($"{i}\t{line.Split('|')[1].Trim()}\t{line.Split('|')[2].Trim()}");
-                            list.Add($"{i++}\t{line.Split('|')[1].Trim()}\t{line.Split('|')[2].Trim()}");
-                        }
+                    string line = "";
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        if (line.Length != 0 && line[0] == '|')
+                            if (!(line.Contains("русский") || line.Contains("-----")))
+                            {
+                                Console.WriteLine($"{i}\t{line.Split('|')[1].Trim()}\t{line.Split('|')[2].Trim()}");
+                                list.Add($"{i++}\t{line.Split('|')[1].Trim()}\t{line.Split('|')[2].Trim()}");
+                            }
+                    }
                 }
             }
-            using(StreamWriter sr = new StreamWriter($@"{Environment.CurrentDirectory}\\wordlist"))
+            using (StreamWriter sr = new StreamWriter($@"{Environment.CurrentDirectory}\\wordlist"))
             {
-                foreach(string line in list)
+                foreach (string line in list)
                 {
                     sr.WriteLine(line);
                 }
             }
+            Console.WriteLine("down!");
             Console.ReadLine();
         }
     }
