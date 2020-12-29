4a5509f4b95711895a1d5cea03ab69c4373d77dc
Ran-ying <62641267+Ran-ying@users.noreply.github.com>
2020-12-29 15:56:24 +0800

    update

--- giveWord/index.php
+++ giveWord/index.php
@@ -14,36 +14,43 @@
     //一天有86400s，+1s！
     $unixTime = time();
     $unixStart = 1609286400;  //2020-12-30
-    $day = (($unixTime-$unixStart)-($unixTime-$unixStart)%86400)/86400;
-    //echo $day;
-    $nowWords = array();
-    $nowWords["day"] = $day;
-    if($day*15-45>=0){
-        for($i=$day*15-45;$i<$day*15-40;$i++){
+    
+    if($unixTime-$unixStart<0)
+    {
+        echo json_encode(array("day"=>"<span style='font-size:3em'>not starting!</span> "));
+    }
+    else{
+        $day = (($unixTime-$unixStart)-($unixTime-$unixStart)%86400)/86400;
+        //echo $day;
+        $nowWords = array();
+        $nowWords["day"] = $day;
+        if($day*15-45>=0){
+            for($i=$day*15-45;$i<$day*15-40;$i++){
+                $nowWords[$i] = $wordArray[$i];
+            }
+        }
+        if($day*15-25>=0){
+            for($i=$day*15-25;$i<$day*15-20;$i++){
+                $nowWords[$i] = $wordArray[$i];
+            }
+        }
+        if($day*15-5>=0){
+            for($i=$day*15-5;$i<$day*15;$i++){
+                $nowWords[$i] = $wordArray[$i];
+            }
+        }
+        for($i=$day*15;$i<$day*15+15;$i++){
             $nowWords[$i] = $wordArray[$i];
         }
-    }
-    if($day*15-25>=0){
-        for($i=$day*15-25;$i<$day*15-20;$i++){
+        for($i=$day*15+15;$i<$day*15+20;$i++){
             $nowWords[$i] = $wordArray[$i];
         }
-    }
-    if($day*15-5>=0){
-        for($i=$day*15-5;$i<$day*15;$i++){
+        for($i=$day*15+35;$i<$day*15+40;$i++){
             $nowWords[$i] = $wordArray[$i];
         }
+        for($i=$day*15+55;$i<$day*15+60;$i++){
+            $nowWords[$i] = $wordArray[$i];
+        }
+        echo json_encode($nowWords);
     }
-    for($i=$day*15;$i<$day*15+15;$i++){
-        $nowWords[$i] = $wordArray[$i];
-    }
-    for($i=$day*15+15;$i<$day*15+20;$i++){
-        $nowWords[$i] = $wordArray[$i];
-    }
-    for($i=$day*15+35;$i<$day*15+40;$i++){
-        $nowWords[$i] = $wordArray[$i];
-    }
-    for($i=$day*15+55;$i<$day*15+60;$i++){
-        $nowWords[$i] = $wordArray[$i];
-    }
-    echo json_encode($nowWords);
 ?>
